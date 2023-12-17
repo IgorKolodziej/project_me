a27f7a1f8dd631bfb366a72a8bda86b081e3cbf3
Igor Kołodziej <igor.kolodziej@proton.me>
2023-12-17 18:24:56 +0100

    CI

---
+++ README.md
@@ -0,0 +1,96 @@
+# Project Me
+
+## Description
+
+Provide a brief description of your project here.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- Python 3.11
+- Poetry
+- Pre-commit
+
+### Installing
+
+1. **Clone the repository**
+
+\`\`\`bash
+git clone https://github.com/yourusername/your-repo-name.git
+cd your-repo-name
+\`\`\`
+
+2. **Set up Python environment**
+
+You need Python 3.11 installed on your system. You can download it from the official website:
+
+https://www.python.org/downloads/
+
+3. **Install Poetry**
+
+Poetry is a tool for dependency management and packaging in Python. You can install it by running:
+
+\`\`\`bash
+pip install poetry
+\`\`\`
+
+4. **Install dependencies**
+
+With Poetry installed, you can install all dependencies by running:
+
+\`\`\`bash
+poetry install
+\`\`\`
+
+5. **Install pre-commit**
+
+Pre-commit is a tool to manage Git hooks. You can install it by running:
+
+\`\`\`bash
+poetry run pip install pre-commit
+\`\`\`
+
+6. **Run pre-commit hooks**
+
+You can run all pre-commit hooks on all files by running:
+
+\`\`\`bash
+poetry run pre-commit run --all-files
+\`\`\`
+
+## Running the tests
+
+Explain how to run the automated tests for this system.
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system.
+
+## Built With
+
+* Dash - The web framework used
+* Poetry - Dependency Management
+* Pre-commit - Git hooks manager
+
+## Contributing
+
+Please read `CONTRIBUTING.md` for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+* **Your Name** - *Initial work* - YourUsername
+
+See also the list of contributors who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the `LICENSE.md` file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
--- project_me/app.py
+++ project_me/app.py
@@ -1,48 +1,62 @@
 # Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import dash_mantine_components as dmc
 import pandas as pd
 import plotly.express as px
-import dash_mantine_components as dmc
+from dash import Dash, Input, Output, callback, dash_table, dcc
 
 # Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-
-# Initialize the app - incorporate a Dash Mantine theme
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"  # noqa:E501
+)  # Initialize the app - incorporate a Dash Mantine theme
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # App layout
-app.layout = dmc.Container([
-    dmc.Title('My First App with Data, Graph, and Controls', color="blue", size="h3"),
-    dmc.RadioGroup(
-        [dmc.Radio(i, value=i) for i in ['pop', 'lifeExp', 'gdpPercap']],
-        id='my-dmc-radio-item',
-        value='lifeExp',
-        size="sm"
-    ),
-    dmc.Grid([
-        dmc.Col([
-            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
-        ], span=6),
-        dmc.Col([
-            dcc.Graph(figure={}, id='graph-placeholder')
-        ], span=6),
-    ]),
-
-], fluid=True)
+app.layout = dmc.Container(
+    [
+        dmc.Title(
+            "My First App with Data, Graph, and Controls",
+            color="blue",
+            size="h3",  # noqa:E501
+        ),
+        dmc.RadioGroup(
+            [dmc.Radio(i, value=i) for i in ["pop", "lifeExp", "gdpPercap"]],
+            id="my-dmc-radio-item",
+            value="lifeExp",
+            size="sm",
+        ),
+        dmc.Grid(
+            [
+                dmc.Col(
+                    [
+                        dash_table.DataTable(
+                            data=df.to_dict("records"),
+                            page_size=12,
+                            style_table={"overflowX": "auto"},
+                        )
+                    ],
+                    span=6,
+                ),
+                dmc.Col(
+                    [dcc.Graph(figure={}, id="graph-placeholder")], span=6
+                ),  # noqa:E501
+            ]
+        ),
+    ],
+    fluid=True,
+)
 
 
 # Add controls to build the interaction
 @callback(
-    Output(component_id='graph-placeholder', component_property='figure'),
-    Input(component_id='my-dmc-radio-item', component_property='value')
+    Output(component_id="graph-placeholder", component_property="figure"),
+    Input(component_id="my-dmc-radio-item", component_property="value"),
 )
 def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    fig = px.histogram(df, x="continent", y=col_chosen, histfunc="avg")
     return fig
 
 
 # Run the App
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
